05c86d13834a61a0e2097381bf9ea7c6ba1c0126
wangge <1727084024.qq.com>
2019-11-24 21:38:34 +0800

    项目部署到gh上

---
+++ precache-manifest.397d15f6c95635666601ea20bc07b4fc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64726acf129491438a3",
+    "url": "/css/app.502f81b8.css"
+  },
+  {
+    "revision": "f6e7134ac250e0f8e439",
+    "url": "/css/chunk-vendors.57992e24.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "ec9bf219195e61f87520805f67014422",
+    "url": "/img/login_bg.ec9bf219.jpg"
+  },
+  {
+    "revision": "e08922597fac9988710975a4f4ce50ad",
+    "url": "/img/logo_index.e0892259.png"
+  },
+  {
+    "revision": "f7f7f50e9560cc524917730c0dfc1502",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e64726acf129491438a3",
+    "url": "/js/app.5dcfd3cf.js"
+  },
+  {
+    "revision": "f6e7134ac250e0f8e439",
+    "url": "/js/chunk-vendors.1dbca647.js"
+  },
+  {
+    "revision": "9924fd334c9b98a18b23b2f8b220c742",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
